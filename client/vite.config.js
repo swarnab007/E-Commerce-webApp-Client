c1eda5b3b7294acc7a30c4a3d453ccbe6ff0d92d
Swarnab Banerjee <116362013+swarnab007@users.noreply.github.com>
2024-06-05 23:12:56 +0530

    backend url added

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server: {
     proxy: {
-      '/api': "http://localhost:7000"
+      '/api': "https://ecommerse-server01.azurewebsites.net"
     }
   },
   plugins: [react()],
