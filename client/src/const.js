2bc4472412a55a3c6973520faa4c9a03891d1652
Swarnab Banerjee <116362013+swarnab007@users.noreply.github.com>
2024-06-05 23:54:56 +0530

    server url changed

--- client/src/const.js
+++ client/src/const.js
@@ -1,2 +1,2 @@
-//export const SERVER_URL = "https://ecommerse-server01.azurewebsites.net";
-export const SERVER_URL="http://localhost:7000";
+export const SERVER_URL = "https://ecommerse-server01.azurewebsites.net";
+//export const SERVER_URL="http://localhost:7000";
