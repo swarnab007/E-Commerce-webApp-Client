99400a7ea7a1058885fc71c028bae44cfe7fec35
Swarnab Banerjee <116362013+swarnab007@users.noreply.github.com>
2024-06-06 00:05:34 +0530

    server removed

--- server/utils/helper.js
+++
@@ -1,6 +0,0 @@
-const bcrypt = require('bcrypt');
-
-exports.comparePassword = async (password, hashedPassword) => {
-    const isValid = await bcrypt.compare(password, hashedPassword);
-    return isValid;
-}
